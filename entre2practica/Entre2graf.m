b2e22c099a76996a50c414069b7a2afe5d459896
joackoodeg <joackoodegiovanni@gmail.com>
2025-04-27 19:20:44 -0300

    grafica de 2023

--- entre2practica/Entre2graf.m
+++ entre2practica/Entre2graf.m
@@ -8,8 +8,21 @@ pt=3.5;
 f = @(x) ((x./(1-x)).*sqrt((2*pt)./(2+x))) - K;
 
 %Grafica
-t=linspace(-1.99,6,200);
+figure(1);
+t=linspace(0,1,1000);
 hold on;
-y = @(t) t.*0;
 grid on;
-plot(t,y(t),'-k');
+plot(t,f(t));
+ylim([-1,1]);
+title("a)");
+
+%----------------------B----------------------%
+x=0.02;
+g = @(p) ((x./(1-x)).*sqrt((2*p)./(2+x))) - K;
+
+figure(2);
+w=linspace(0,5,1000);
+hold on;
+grid on;
+plot(w,g(w));
+title("b)");
